69cc9a41d3158c73e30163f172c671c8ae3eae30
Aarushi Garg <aarushigarg14@gmail.com>
2025-11-23 20:11:19 +0530

    Changed Celsius->Fahrenheit formula differently

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 def convert(value, scale):
     if scale == "C":
-        return (value * 9/5) + 32   # This line will be changed in both branches
+           return (value * 9/5) + 30
     elif scale == "F":
         return (value - 32) * 5/9   # This line will also be changed differently
     else:
